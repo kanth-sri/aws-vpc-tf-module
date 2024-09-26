7b863d968377dc83deead1c5715304d4cf17f470
kanth-sri <srikanthadepu8008@gmail.com>
2024-09-26 10:27:41 +0530

    aws vpc tf custom module

--- output.tf
+++ output.tf
@@ -4,3 +4,15 @@
 # output "default_vpc" {
 #   value = data.aws_vpc.default
 # }
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+output "private_subent_ids" {
+  value = aws_subnet.private[*].id
+}
+output "db_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
